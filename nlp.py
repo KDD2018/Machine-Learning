ef22926497bcf0210278198214e9c9cd26225e39
KDD2018 <935796564.qq.com>
2018-12-05 20:08:40 +0800

    update nlp

--- nlp.py
+++ nlp.py
@@ -2,63 +2,152 @@
 # -*- coding: utf-8 -*-
 
 import jieba
+from jieba import posseg as psg
+from jieba import analyse
 import nltk
 from wordcloud import WordCloud
+from wordcloud import ImageColorGenerator as Imag
 import matplotlib.pyplot as plt
+import docx
 
-# 获取文本数据
-def get_text(fila_name):
-    f = open(fila_name, 'r', encoding='utf-8')
+
+def get_text(file_name):
+    '''
+    Desc: 读取文本内容
+    Args: file_name 文本文件位置
+    Ｒeturns: text(str) 文本内容
+    '''
+
+    f = open(file_name, 'r', encoding='utf-8')
     text = f.read()
     f.close()
     return text
 
-# 分词并除去停用词
+def get_doc(file_name):
+    '''
+    Desc: 读取Word文档
+    Args: file_name 文档位置
+    Returns: document(str) 文档字符串
+    '''
+
+    document = ''
+    file = docx.Document(file_name)
+    for paragraph in file.paragraphs:
+        graph = paragraph.text.replace(' ', '')  # 除去段内空格
+        document += graph + '\n'
+    return document
+
+def doc2sent(doc):
+    '''
+    Desc: 除去文档中的空行并组成字符串
+    Args: doc 文档字符串
+    Returns: sents(str) 句子字符串
+    '''
+    sentences = doc.split('\n')  # 切分成句子
+    sents = ''
+    for sent in sentences:
+        if sent == '':
+            continue
+        sents += sent + '\n'
+    return sents
+
 def sent2word(sentence, stopwords):
     """
-    Segment a sentence to words
-    Delete stopwords
+    Desc: 分词并除去停用词
+    Args: sentence 待分词的文本内容
+          stopwords 停用词
+    Returns: word_str(list) 分词结果
     """
+
+    jieba.load_userdict('/home/kdd/userdict.txt')
     segResult = jieba.lcut(sentence)
-    newSent = []
+    word_str = ''
     for word in segResult:
         if word in stopwords:
             continue
         elif word.isspace():
             continue
         else:
-            newSent.append(word)
-    return newSent
+            # newSent.append(word)
+            word_str += word + ' '  
+    return word_str
 
-text = get_text('/home/kdd/nlp/beauti.txt')  # 获取文本数据
-stopWords = get_text('/home/kdd/nlp/stop_words.txt')  # 停用词
+def pos_tag(word_str):
+    # 词性标注
+    word_pog = psg.cut(word_str)
+    tag = {}
+    for w in word_pog:
+        # print(w.word, w.flag)
+        tag[w.word] = w.flag
+    return tag
 
-# sents = nltk.word_tokenize(text)
+def draw_cloud(mask, word_freq):
+    '''
+    Desc: 根据词频绘制词云图
+    Args: mask 绘制图形形状
+          word_freq 词频字典
+    '''
 
-# 分词
-words = sent2word(text, stopWords)
+    word_cloud = WordCloud(font_path='/usr/share/fonts/windows/msyh.ttf', # 字体
+                           # background_color='white',   #背景颜色
+                           width=1000,
+                           height=600,
+                           max_font_size=50,            #字体大小
+                           min_font_size=10,
+                           mask=mask,                   #背景图片
+                           max_words=400,
+                           scale = 1.8)
+    word_cloud = word_cloud.fit_words(word_freq)
+    word_cloud.to_file('wordcloud.png')
+    plt.figure('Word Cloud')
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.show()
 
 
-# 统计词频
-fd = nltk.FreqDist(words)
-oft = fd.most_common(400)
+# 自定义文本位置
+text_file = '/home/share/业务约定书.docx'
+stopwords_file = '/home/kdd/stop_words.txt'
 
-# print(oft)
+# 自定义词云图背景
+mask = plt.imread('heart.jpeg')
 
 
-# 绘制词云图
-wc = WordCloud(font_path='/usr/share/fonts/wps-office/simhei.ttf',
-               # background_color='white',   #背景颜色
-               width=1000,
-               height=600,
-               max_font_size=50,            #字体大小
-               min_font_size=10,
-               # mask=plt.imread('xin.jpg'),  #背景图片
-               max_words=400)
-wc.generate(text)
-wc.to_file('luyun.png')
 
-plt.figure('word cloud')
-plt.imshow(wc)
-plt.axis('off')
-plt.show()
+if __name__ == '__main__':
+
+    # 获取文本和停用词
+    # text = get_text(file_name=text_file)
+    doc = get_doc(text_file)
+    stopWords = get_text(file_name=stopwords_file)
+    # print(doc)
+    
+    # 切分成句子
+    sents = doc2sent(doc)  
+    # print(sents)
+
+    # 分词、统计词频
+    words = sent2word(sents, stopWords)
+    word_freq = dict(nltk.FreqDist(words.split(' ')).most_common(25))
+    # print(words)
+
+    # oft = fd.most_common(200)l
+    # word_freq = dict(oft)
+    print(word_freq)
+
+    # 绘制词云图
+    # draw_cloud(mask, word_freq)
+    # tag = pos_tag(words) # 词性标注
+    # print(tag)
+
+    # 关键词抽取
+    topic = analyse.extract_tags(words, topK=25)
+    print('基于TF-IDF算法: \n', topic)
+    topic = analyse.textrank(words, topK=25)
+    print('\n基于TextRank算法: \n', topic)
+
+
+
+
+
+
