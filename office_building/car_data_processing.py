e861a59051b065523ca84fec8c4839cb9c1d1bcd
吴程锦 <935796564@qq.com>
2019-05-22 20:33:18 +0800

    update car data process

--- office_building/car_data_processing.py
+++ office_building/car_data_processing.py
@@ -5,6 +5,7 @@ import pymongo
 import math
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import pymysql
 from datetime import datetime
 from sklearn.preprocessing import LabelEncoder
@@ -54,20 +55,9 @@ def boxplot(data):
     :param data: 数值型Series
     :return: 返回异常点及其位置
     '''
-    plt.boxplot(x=data,  # 指定绘制箱线图的数据
-                whis=3,  # 指定1.5倍的四分位差
-                # notch=True,
-                widths=0.8,  # 指定箱线图的宽度为0.8
-                patch_artist=True,  # 指定需要填充箱体颜色
-                showmeans=True,  # 指定需要显示均值
-                boxprops={'facecolor': 'steelblue'},  # 指定箱体的填充色为铁蓝色
-                # 指定异常点的填充色、边框色和大小
-                flierprops={'markerfacecolor': 'red', 'markeredgecolor': 'red', 'markersize': 2},
-                # 指定均值点的标记符号（菱形）、填充色和大小
-                meanprops={'marker': 'D', 'markerfacecolor': 'black', 'markersize': 4},
-                medianprops={'linestyle': '--', 'color': 'orange'},
-                # 指定中位数的标记符号（虚线）和颜色
-                labels=[''])  # 去除箱线图的x轴刻度值
+    sns.set(style='darkgrid')
+    plt.rcParams['font.sans-serif'] = ['Microsoft Yahei']
+    sns.catplot(x='compulsory_insurance', y='car_price', kind='box', data=data)
     plt.show()
     Q1 = data.quantile(0.25)
     Q3 = data.quantile(0.75)
@@ -78,27 +68,27 @@ def boxplot(data):
     return low_whisker, up_whisker
 
 
-def line_plot(data):
+def scatter_plot(data):
     '''
-    通过3sigma原则去除异常值
+    绘制散点图
     :param data: 数值型Series
     :return: low_whisker 异常下界
              up_whisker 异常上界
     '''
-    plt.plot(
-             data,  # y轴数据
-             linestyle='-',  # 设置折线类型
-             linewidth=2,  # 设置线条宽度
-             color='steelblue',  # 设置折线颜色
-             marker='o',  # 往折线图中添加圆点
-             markersize=4,  # 设置点的大小
-             markeredgecolor='black',  # 设置点的边框色
-             markerfacecolor='black')  # 设置点的填充色
+    plt.rcParams['font.sans-serif'] = ['Microsoft Yahei']
+    plt.scatter(x=data['meter_future_rate'],
+                y=data['car_price'],  # y轴数据
+                c='g',  # 设置颜色
+                # s=0.9,
+                alpha=0.7)
+    plt.xlabel('meter_future_rate')
+    plt.ylabel('car_price')
+
     # 添加上下界的水平参考线（便于判断异常点，如下判断极端异常点，只需将2改为3）
-    low_whisker = data.mean() - 3 * data.std()
-    up_whisker = data.mean() + 3 * data.std()
-    plt.axhline(y=low_whisker, linestyle='--', color='gray')
-    plt.axhline(y=up_whisker, linestyle='--', color='gray')
+    low_whisker = data['car_price'].mean() - 3 * data['car_price'].std()
+    up_whisker = data['car_price'].mean() + 3 * data['car_price'].std()
+    # plt.axhline(y=low_whisker, linestyle='--', color='gray')
+    # plt.axhline(y=up_whisker, linestyle='--', color='gray')
     plt.show()
 
     return low_whisker, up_whisker
@@ -125,23 +115,23 @@ def preprocess(data):
     :param data: 要处理的数据框
     :return: 处理后的数据框
     '''
-
-    data['risk_27_check'] = data['risk_27_check'].fillna('None')
-    data = data[data['risk_27_check'].isin(['None', '0'])]  # 去除重大事故的车
     data = data[data['sell_times'] < 5]  # 去除过户次数大于4次的二手车
-    data['meter_mile'] = data['meter_mile'] / 10000.0  # 换算成万公里
-    data['car_price'] = data['car_price'] / 10000.0  # 换算成万元
+    data['meter_mile'] = data.loc[:, 'meter_mile'] / 10000.0  # 换算成万公里
+    data['car_price'] = data.loc[:, 'car_price'] / 10000.0  # 换算成万元
+    data = data[data.meter_mile < 50]  # 过滤掉50万公里以上的案例
+    # data = data[data.car_price > 1]  # 过滤掉车价低于１万的
     data['meter_future_rate'] = 1 - data['meter_mile'] / 60  # 转换成行驶里程成新率
     data = data.dropna()
     # print(data.isnull().any())
     data['model_year'] = data['model_year'].map(lambda x: str(x))
     data['annual_inspect'] = data['annual_inspect'].map(
-        lambda x: (datetime.now().year - x.year) * 12 + (datetime.now().month - x.month))
-    data['compulsory_insurance'] = data['compulsory_insurance'].map(
-        lambda x: (datetime.now().year - x.year) * 12 + (datetime.now().month - x.month))
+        lambda x: ((x.year - datetime.now().year) * 12 + (x.month - datetime.now().month)) / 12)
+    # data['compulsory_insurance'] = data['compulsory_insurance'].map(
+    #     lambda x: ((x.year - datetime.now().year) * 12 + (x.month - datetime.now().month)) / 12)
     data['car_age'] = data['register_time'].map(
         lambda x: ((datetime.now().year - x.year) * 12 + (datetime.now().month - x.month)) / 12)
-    # data = data[data.car_price < data['car_price'].max()]
+    data['car_price'] = data['car_price'].map(lambda x: math.log(x))
+
     return data
 
 
@@ -151,12 +141,14 @@ def feature_encode(data):
     :param data: 数据框
     :return: 特征编码后的数据框
     '''
-    # data['displacement'] = pd.cut(data.displacement, bins=[-1, 0.01, 1, 1.6, 2, 3, 4, 8],
-    #                               labels=['0L', '0.01-1L', '1-1.6L', '1.6-2L', '2-3L', '3-4L', '4L以上'])  # 排量
+    data['displacement'] = pd.cut(data.displacement, bins=[-1, 0.01, 1, 1.6, 2, 3, 4, 8],
+                                  labels=['0L', '0.01-1L', '1-1.6L', '1.6-2L', '2-3L', '3-4L', '4L以上'])  # 排量
     data['car_age'] = pd.cut(data.car_age, bins=[-1, 1, 3, 5, 8, 50],
                              labels=['1年以内', '1-3年', '3-5年', '5-8年', '8年以上'])  # 车龄
     data['sell_times'] = pd.cut(data.sell_times, bins=[-1, 0.1, 1.1, 2.1, 3.1, 4.1],
                                 labels=['0次', '1次', '2次', '3次', '4次'])  # 过户次数
+    data['annual_inspect'] = pd.cut(data.annual_inspect, bins=[-20, -10, -5, 0, 5],
+                                          labels=['10年以前', '5年以前', '过去5年', '未来5年'])
     return data
 
 
@@ -179,15 +171,19 @@ def split_data(data):
     :param data: 数据框
     :return: X_train, X_test, y_train, y_test
     '''
-    target = data.iloc[:,-1]
+    target = data.iloc[:,-1][:-1]
     feature_X = data.iloc[:,:-1]
     feature = pd.get_dummies(feature_X)  # 哑变量编码
+    my_car_info = pd.DataFrame([feature.iloc[-1, :]])
+    feature = feature.iloc[:-1, :]
+    # print(feature_X.head())
     X_train, X_test_data, y_train, y_test_data = train_test_split(feature, target, test_size=0.3)
     X_test, X_valid, y_test, y_valid = train_test_split(X_test_data, y_test_data, test_size=0.3)
-    return X_train, X_test, X_valid, y_train, y_test, y_valid
+
+    return X_train, X_test, X_valid, y_train, y_test, y_valid, my_car_info
 
 
-def model_select(model_name, X_train, X_test, X_valid, y_train, y_test, y_valid):
+def model_select(model_name, X_train, X_test, X_valid, y_train, y_test, y_valid, my_car_info):
     '''
     确定最优模型
     :param model_name: 模型名称 ['SVR', 'CART', 'RF', 'GBR']
@@ -213,11 +209,44 @@ def model_select(model_name, X_train, X_test, X_valid, y_train, y_test, y_valid)
     print('\n**************平均绝对误差为：%f**************' %abe)
     # print(regressor.feature_importances_)
 
-    plt.scatter( y_valid, y_hat -y_valid)
+    plt.scatter( y_valid, y_hat)
     plt.xlabel('y_valid_true')
-    plt.ylabel('residual')
+    plt.ylabel('y_hat')
     plt.grid()
     plt.show()
+
+    log_price = regressor.predict(my_car_info)
+    your_car_price = math.exp(log_price)
+    print('\n**************您的爱车值这个价：%f**************' %your_car_price)
+
+
+def my_car():
+    '''
+    获取预估车辆信息
+    :return: 用户车辆表
+    '''
+    brand_name = input('请输入品牌：')
+    car_system = input('请输入车系：')
+    car_model_name = input('请输入车型：')
+    register_time = datetime.strptime(input('请输入上牌时间：'), '%Y-%m-%d')
+    meter_mile = float(input('请输入已行驶里程（公里）：'))
+    gearbox = input('请输入变速箱类型：')
+    displacement = float(input('请输入排量(L)：'))
+    sell_times = float(input('请输入过户次数：'))
+    annual_inspect = datetime.strptime(input('请输入年检到期时间：'), '%Y-%m-%d')
+    compulsory_insurance = datetime.strptime(input('请输入交强险的到期时间：'), '%Y-%m-%d')
+    model_year = input('请输入车款年份：')
+    car_level = input('请输入车辆级别：')
+
+    car_info = {'brand_name': brand_name, 'car_system': car_system, 'car_model_name': car_model_name,
+                'register_time': register_time, 'meter_mile': meter_mile, 'gearbox': gearbox,
+                'displacement': displacement, 'sell_times': sell_times, 'annual_inspect': annual_inspect,
+                'compulsory_insurance': compulsory_insurance, 'model_year': model_year, 'car_level': car_level,
+                'car_price': 0.1}
+    my_car_info = pd.DataFrame([car_info])
+
+    return my_car_info
+
 
 
 # np.set_printoptions(threshold=np.inf)
@@ -239,7 +268,6 @@ select
 	semiautomatic_gearbox,
 	displacement,	
 	sell_times,
-	risk_27_check,
 	year_check_end_time,
 	is_have_strong_risk,
 	model_year,
@@ -248,31 +276,22 @@ select
 from
 	second_car_sell
 where
-    car_class='saloon'
+    car_class='suv'
+and
+    (risk_27_check = '0'
+or 
+    risk_27_check is null)
 """
 
-col = ['brand_name', 'car_system', 'car_model_name', 'register_time', 'meter_mile', 'gearbox', 'displacement', 'sell_times',
-       'risk_27_check', 'annual_inspect', 'compulsory_insurance', 'model_year', 'car_level', 'car_price']
+col = ['brand_name', 'car_system', 'car_model_name', 'register_time', 'meter_mile', 'gearbox', 'displacement',
+       'sell_times', 'annual_inspect', 'compulsory_insurance', 'model_year', 'car_level', 'car_price']
 
 col1 = ['brand_name', 'car_system', 'car_age', 'meter_future_rate', 'gearbox', 'displacement', 'sell_times',
-        'compulsory_insurance', 'model_year', 'car_price']
+        'annual_inspect', 'model_year', 'car_price']
 
 car_class_dict = {'suv': ['大型SUV', '中大型SUV', '小型SUV', '紧凑型SUV', '中型SUV'],
                   'saloon': ['小型车', '大型车', '微型车', '中大型车', '中型车', '紧凑型车'], 'supercar': '跑车',
                   'pickup': ['皮卡', '微卡', '高端皮卡'], 'mpv': 'MPV', 'minibus':['轻客', '微面']}
-brand_grade_dict = {''}
-
-
-# def brand_grade(data):
-#     '''
-#     将汽车品牌划分档次
-#     :param data: 新车报价数据/二手车案例数据
-#     :return: 添加品牌档次的data
-#     '''
-#     data
-
-
-
 
 
 if __name__ == '__main__':
@@ -280,75 +299,66 @@ if __name__ == '__main__':
     # data = connect_mongo(project)
     # data.to_csv('/home/kdd/Desktop/car.csv', encoding='gbk')  # 写入csv
 
+    # 获取用户车辆信息
+    my_car_info = my_car()
+
     # 获取MySQL数据
-    data = conn_mysql(sql, col)
-    # print(data.head())
+    data_case = conn_mysql(sql, col)
+    data_case = data_case[data_case.car_price > 10000]
+    data = pd.concat([data_case, my_car_info], sort=False, ignore_index=True)
+    # print(data.tail())
 
     # 描述统计分析
     # print(data.describe(include='all'))
     # data.to_csv('/home/kdd/python/car/saloon.csv', encoding='gbk', chunksize=10000)
 
-
     # 数据预处理
     data = preprocess(data)
-    # print(data.head())
 
     # 特征编码
     data = feature_encode(data)
-    print(set(data['car_level']))
+
     # 去除异常
-    # low_whisker, up_whisker =  boxplot(data['meter_mile'])
+    # low_whisker, up_whisker =  boxplot(data)
     # l, u = boxplot(data.car_price)
     # data = data[~(data.meter_mile > up_whisker) | (data.meter_mile < low_whisker)]
-    # low_whisker, up_whisker =  line_plot(data['car_price'])
+    # low_whisker, up_whisker =  scatter_plot(data)
     # data = data[~(data.car_price > u) | (data.car_price < l)]
 
     # 有效字段
     df = data[col1]
     df.index = range(len(df))
-    # print(df.head())
+    print(df.tail(1))
 
-    gb = df['car_price'].groupby(df['brand_name']).mean()
-    brand_grade = {}
-    for brand in gb.index:
-        if gb[brand] >=50:
-            brand_grade[brand] = '豪华'
-        elif gb[brand] >=20:
-            brand_grade[brand] = '高端'
-        elif gb[brand] >=8:
-            gb[brand] = '中端'
-        else:
-            gb[brand] = '低端'
-    print(brand_grade)
-
-    df['car_grade'] = pd.Series(np.random.rand(len(df)))
-    for i in df.index:
-        for k, v in brand_grade.items():
-            if df['brand_name'][i] == k:
-                df['car_grade'][i] = v
-    print(df.head())
-
-
-    # print(df.head())
     # 将数据框分批次写入多个csv
     # write2csv(data=df, batch_size=50000)
     # print(set(df['car_type']))
     # df.to_csv('/home/kdd/python/car/car_mpv.csv', encoding='gbk', chunksize=10000)
 
 
-
-
-
     # 划分数据集
-    # X_train, X_test, X_valid, y_train, y_test, y_valid = split_data(df)
+    X_train, X_test, X_valid, y_train, y_test, y_valid, my_car_info = split_data(df)
     # print(X_train.shape, X_test.shape, X_valid.shape, y_train.shape, y_test.shape)
+    # print(my_car_info)
 
 
     # 建立机器学习模型
-    # model_select('GBR', X_train, X_test, X_valid, y_train, y_test, y_valid)  # 模型名称 ['SVR', 'CART', 'RF', 'GBR']
+    model_select('GBR', X_train, X_test, X_valid, y_train, y_test, y_valid, my_car_info)  # 模型名称 ['SVR', 'CART', 'RF', 'GBR']
 
 
 
-
+    '''
+    # 添加车的档次属性
+    gb = df['car_price'].groupby(df['brand_name']).mean()
+    grade = pd.cut(gb, bins=[0, 8, 20, 50, 1000],
+                   labels=['低端 ', '中端', '高端', '豪华'])
+    car_grade = grade.to_dict()
+    df['grade'] = pd.Series(np.random.rand(len(df)))
+    for i in df.index:
+        # for k, v in car_grade.items():
+        if df['brand_name'][i] in car_grade.keys():
+            df.loc['grade'] = car_grade[df['brand_name'][i]]
+    print(df.head())
+    '''
 
 
